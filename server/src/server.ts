4ec63685a142ec8860dff8eec147e5640c462d4b
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-06-04 08:54:51 -0300

    Adicionado arquivo do server

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import { config } from 'dotenv';
+import express, { json } from 'express';
+import path from 'path'
+import cors from 'cors';
+import routes from './routes';
+
+config();
+const app = express();
+const PORT = process.env.SERVER_PORT;
+
+app.use(cors());
+app.use(json());
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.listen(PORT || 3333, () => {
+    console.log(`Server running at: http://localhost:${PORT}/`);
+});
