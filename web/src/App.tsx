8a7d466c01cb6f74189cd2e3502d7004000946ed
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-06-04 20:56:16 -0300

    Criação da página de formulário

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
-import Home from './pages/Home';
-
+import Routes from './routes';
 function App() {
   return (
-   <Home/>
+    <Routes />
   );
 }
 
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../../assets/logo.svg';
 import './styles.css';
 import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -13,14 +14,14 @@ const Home = () => {
                 <main>
                     <h1>Seu marketplace de coelta de resíduos.</h1>
                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</p>
-                    <a href="/cadastro">
+                    <Link to="/create-point">
                         <span>
-                            <FiLogin />
+                            <FiLogIn />
                         </span>
                         <strong>
                             Cadastre um ponto de coleta
                         </strong>
-                    </a>
+                    </Link>
                 </main>
             </div>
         </div>
