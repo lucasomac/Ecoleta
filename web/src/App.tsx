f4d1eda5f09d096beb4a0d73ec86d333e4d9991f
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-06-04 12:56:48 -0300

    Inserção de imagens de logo e background

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <h1>Hello Word </h1>
+   <Home/>
   );
 }
 
