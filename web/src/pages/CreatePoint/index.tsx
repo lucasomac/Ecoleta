8afd6de52391ae73aacfbde4fdb346c332997511
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-06-04 21:52:15 -0300

    Aplicação pronta para integração com ibge. LI sem funcionar ainda

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../../assets/logo.svg';
 import './styles.css';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
 
+interface Item {
+    id: number;
+    image_url: string;
+    title: string;
+};
 const CreatePoint = () => {
+    const [items, setItems] = useState<Array<Item>>([]);
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, []);
     return (
         <div id="page-create-point">
             <header>
@@ -45,6 +58,15 @@ const CreatePoint = () => {
                             <h2>Endereço</h2>
                             <span>Selecione um endereço no mapa</span>
                         </legend>
+
+                        <Map center={[-10.9076571, -37.0722295]} zoom={15}>
+                            <TileLayer
+                                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <Marker position={[-10.9076571, -37.0722295]} />
+                        </Map>
+
                         <div className="field-group">
                             <div className="field">
                                 <label htmlFor="uf">Estado</label>
@@ -66,30 +88,17 @@ const CreatePoint = () => {
                             <span>Selecione um ou mais items abaixo</span>
                         </legend>
                         <ul className="items-grid">
-                            <li>
-                                <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
-                                <span>Óleo de Cozinha</span>
-                            </li>
-                            <li>
-                                <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
-                                <span>Óleo de Cozinha</span>
-                            </li>
-                            <li>
-                                <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
-                                <span>Óleo de Cozinha</span>
-                            </li>
-                            <li>
-                                <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
-                                <span>Óleo de Cozinha</span>
-                            </li>
-                            <li>
-                                <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
-                                <span>Óleo de Cozinha</span>
-                            </li>
-                            <li>
-                                <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
-                                <span>Óleo de Cozinha</span>
-                            </li>
+                            {items.map(item => (
+                                <li key={item.id} >
+                                    <img src={item.image_url} alt={item.title} />
+                                    <span>{item.title}</span>
+                                </li>))}
+                            {/* // ))}
+                            // <li>
+                            //     <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
+                            //     <span>Óleo de Cozinha</span>
+                            // </li> */}
+
                         </ul>
                     </fieldset>
                     <button type="submit">
